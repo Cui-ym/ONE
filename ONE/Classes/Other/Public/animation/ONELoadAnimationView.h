2a6fe29e01b0bd20e1c64cd80530d06626136038
Cui-ym <1652923531@qq.com>
2017-12-19 22:42:38 +0800

    /Users/cuiyiming/Desktop/项目/ONE

---
+++ ONE/Classes/Other/Public/animation/ONELoadAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  ONELoadAnimationView.h
+//  ONE
+//
+//  Created by 崔一鸣 on 2017/12/19.
+//  Copyright © 2017年 崔一鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONELoadAnimationView : UIView
+
+@end
---
+++ ONE/Classes/Other/Public/animation/ONELoadAnimationView.m
@@ -0,0 +1,21 @@
+//
+//  ONELoadAnimationView.m
+//  ONE
+//
+//  Created by 崔一鸣 on 2017/12/19.
+//  Copyright © 2017年 崔一鸣. All rights reserved.
+//
+
+#import "ONELoadAnimationView.h"
+
+@implementation ONELoadAnimationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
